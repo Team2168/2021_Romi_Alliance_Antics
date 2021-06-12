6210c4fce58d40b8e132de8a5ce84a4b006c8d0f
sloshyyy <nate@jankowskis.net>
2021-06-11 22:34:02 -0400

    THIS PATH WORKS USE IT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -15,17 +15,6 @@ import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
-import frc.robot.Constants.AutoConstants;
-import frc.robot.Constants.DriveConstants;
-import frc.robot.commands.ArcadeDrive;
-import frc.robot.commands.AutonomousDistance;
-import frc.robot.commands.AutonomousTime;
-import frc.robot.commands.PathConverter;
-import frc.robot.commands.ZeroAllTheThings;
-import frc.robot.subsystems.Drivetrain;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.OnBoardIO;
-import frc.robot.subsystems.OnBoardIO.ChannelMode;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
@@ -36,15 +25,24 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RamseteCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.Button;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.commands.ArcadeDrive;
+import frc.robot.commands.PathConverter;
+import frc.robot.commands.ZeroAllTheThings;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.OnBoardIO;
+import frc.robot.subsystems.OnBoardIO.ChannelMode;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
@@ -58,23 +56,20 @@ public class RobotContainer {
   // Create SmartDashboard chooser for autonomous routines
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
-  private DifferentialDriveVoltageConstraint autoVoltageConstraint =
-  new DifferentialDriveVoltageConstraint(
-      new SimpleMotorFeedforward(DriveConstants.ksVolts, 
-                                 DriveConstants.kvVoltSecondsPerMeter, 
-                                 DriveConstants.kaVoltSecondsSquaredPerMeter),
-      DriveConstants.kDriveKinematics,
-      10);
-  private TrajectoryConfig config =
-        new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond, 
-                            AutoConstants.kMaxAccelerationMetersPerSecondSquared)
-            .setKinematics(DriveConstants.kDriveKinematics)
-            .addConstraint(autoVoltageConstraint);
+  private DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
+      new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
+          DriveConstants.kaVoltSecondsSquaredPerMeter),
+      DriveConstants.kDriveKinematics, 10);
+  private TrajectoryConfig config = new TrajectoryConfig(AutoConstants.kMaxSpeedMetersPerSecond,
+      AutoConstants.kMaxAccelerationMetersPerSecondSquared).setKinematics(DriveConstants.kDriveKinematics)
+          .addConstraint(autoVoltageConstraint);
 
-
-  // NOTE: The I/O pin functionality of the 5 exposed I/O pins depends on the hardware "overlay"
-  // that is specified when launching the wpilib-ws server on the Romi raspberry pi.
-  // By default, the following are available (listed in order from inside of the board to outside):
+  // NOTE: The I/O pin functionality of the 5 exposed I/O pins depends on the
+  // hardware "overlay"
+  // that is specified when launching the wpilib-ws server on the Romi raspberry
+  // pi.
+  // By default, the following are available (listed in order from inside of the
+  // board to outside):
   // - DIO 8 (mapped to Arduino pin 11, closest to the inside of the board)
   // - Analog In 0 (mapped to Analog Channel 6 / Arduino Pin 4)
   // - Analog In 1 (mapped to Analog Channel 2 / Arduino Pin 20)
@@ -83,24 +78,22 @@ public class RobotContainer {
   //
   // Your subsystem configuration should take the overlays into account
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
   }
 
   private Command generateRamseteCommand(Trajectory trajectory) {
-    RamseteCommand ramseteCommand = new RamseteCommand(
-        trajectory,
-        m_drivetrain::getPose,
+    RamseteCommand ramseteCommand = new RamseteCommand(trajectory, m_drivetrain::getPose,
         new RamseteController(AutoConstants.kRamseteB, AutoConstants.kRamseteZeta),
-        new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter, DriveConstants.kaVoltSecondsSquaredPerMeter),
-        DriveConstants.kDriveKinematics,
-        m_drivetrain::getWheelSpeeds,
-        new PIDController(DriveConstants.kPDriveVel, 0, 0),
-        new PIDController(DriveConstants.kPDriveVel, 0, 0),
-        m_drivetrain::tankDriveVolts,
-        m_drivetrain);
+        new SimpleMotorFeedforward(DriveConstants.ksVolts, DriveConstants.kvVoltSecondsPerMeter,
+            DriveConstants.kaVoltSecondsSquaredPerMeter),
+        DriveConstants.kDriveKinematics, m_drivetrain::getWheelSpeeds,
+        new PIDController(DriveConstants.kPDriveVel, 0, 0), new PIDController(DriveConstants.kPDriveVel, 0, 0),
+        m_drivetrain::tankDriveVolts, m_drivetrain);
 
     m_drivetrain.resetOdometry(trajectory.getInitialPose());
 
@@ -112,21 +105,16 @@ public class RobotContainer {
 
         // Finally, we make sure that the robot stops
         .andThen(new InstantCommand(() -> m_drivetrain.tankDriveVolts(0, 0), m_drivetrain));
-  } 
-  
+  }
+
   private Command course1() {
     Trajectory course1 = TrajectoryGenerator.generateTrajectory(
         // Start at the origin facing the +X direction
         new Pose2d(0, 0, new Rotation2d(0)),
 
         // UPDATE THESE POINTS
-        List.of(
-            new Translation2d(0.5, 0.25),
-            new Translation2d(1.0, -0.25),
-            new Translation2d(1.5, 0)
-        ),
-        new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
-        config);
+        List.of(new Translation2d(0.5, 0.25), new Translation2d(1.0, -0.25), new Translation2d(1.5, 0)),
+        new Pose2d(0.0, 0, new Rotation2d(Math.PI)), config);
 
     return generateRamseteCommand(course1);
   }
@@ -137,22 +125,17 @@ public class RobotContainer {
         new Pose2d(0, 0, new Rotation2d(0)),
 
         // UPDATE THESE POINTS
-        List.of(
-            new Translation2d(0.5, 0.25),
-            new Translation2d(1.0, -0.25),
-            new Translation2d(1.5, 0)
-        ),
-        new Pose2d(0.0, 0, new Rotation2d(Math.PI)),
-        config);
+        List.of(new Translation2d(0.5, 0.25), new Translation2d(1.0, -0.25), new Translation2d(1.5, 0)),
+        new Pose2d(0.0, 0, new Rotation2d(Math.PI)), config);
 
     return generateRamseteCommand(course2);
   }
 
   /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   * Use this method to define your button->command mappings. Buttons can be
+   * created by instantiating a {@link GenericHID} or one of its subclasses
+   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
     // Default command is arcade drive. This will run unless another command
@@ -163,36 +146,43 @@ public class RobotContainer {
 
     // Example of how to use the onboard IO
     Button onboardButtonA = new Button(m_onboardIO::getButtonAPressed);
-    onboardButtonA
-        .whenActive(new PrintCommand("Button A Pressed"))
-        .whenInactive(new PrintCommand("Button A Released"));
+    onboardButtonA.whenActive(new PrintCommand("Button A Pressed")).whenInactive(new PrintCommand("Button A Released"));
 
     gyroResetButton.whenPressed(new ZeroAllTheThings(m_drivetrain));
 
-    intakeButton
-      .whenPressed(new InstantCommand(m_intakeMotor::openIntake, m_intakeMotor))
-      .whenReleased(new InstantCommand(m_intakeMotor::closeIntake, m_intakeMotor));
+    intakeButton.whenPressed(new InstantCommand(m_intakeMotor::openIntake, m_intakeMotor))
+        .whenReleased(new InstantCommand(m_intakeMotor::closeIntake, m_intakeMotor));
 
     // Setup SmartDashboard options
-    m_chooser.setDefaultOption("Ramsete Trajectory - Unnamed", new PathConverter(m_drivetrain, "output/Unnamed.wpilib.json").getCommand());
-    m_chooser.addOption("Ramsete Trajectory - Course1", new PathConverter(m_drivetrain, "output/Course1.wpilib.json").getCommand());
-    m_chooser.addOption("Ramsete Trajectory - Course2", new PathConverter(m_drivetrain, "output/Course2.wpilib.json").getCommand());
-    m_chooser.addOption("Ramsete Trajectory - 1 Ball top", new PathConverter(m_drivetrain, "output/1ball_top.wpilib.json").getCommand());
-    m_chooser.addOption("Ramsete Trajectory - 1 Ball bottom", new PathConverter(m_drivetrain, "output/1ball_bottom.wpilib.json").getCommand());
-    m_chooser.addOption("Ramsete Trajectory - park (0 ball)", new PathConverter(m_drivetrain, "output/0ball.wpilib.json").getCommand());
+    // m_chooser.setDefaultOption("Ramsete Trajectory - Unnamed", new
+    // PathConverter(m_drivetrain, "output/Unnamed.wpilib.json").getCommand());
+    // m_chooser.addOption("Ramsete Trajectory - Course1", new
+    // PathConverter(m_drivetrain, "output/Course1.wpilib.json").getCommand());
+    // m_chooser.addOption("Ramsete Trajectory - Course2", new
+    // PathConverter(m_drivetrain, "output/Course2.wpilib.json").getCommand());
+    // m_chooser.addOption("Ramsete Trajectory - 1 Ball top", new
+    // PathConverter(m_drivetrain, "output/1ball_top.wpilib.json").getCommand());
+    // m_chooser.addOption("Ramsete Trajectory - 1 Ball bottom", new
+    // PathConverter(m_drivetrain, "output/1ball_bottom.wpilib.json").getCommand());
 
-    m_chooser.addOption("Ramsete Trajectory - COMMAND GROUUUUUUUUUUUUUUPPPPPPPPPJ!!!!!!!!!!!!!!!!!!!", new SequentialCommandGroup(
-      new PathConverter(m_drivetrain, "output/1ball_bottom.wpilib.json").getCommand(),
-      new InstantCommand(m_intakeMotor::openIntake, m_intakeMotor),
-      new PathConverter(m_drivetrain, "output/bottomtopark.wpilib.json").getCommand(),
-      new InstantCommand(m_intakeMotor::closeIntake, m_intakeMotor)
-    ));
+    // m_chooser.addOption("Ramsete Trajectory - COMMAND
+    // GROUUUUUUUUUUUUUUPPPPPPPPPJ!!!!!!!!!!!!!!!!!!!", new SequentialCommandGroup(
+    // new PathConverter(m_drivetrain,
+    // "output/1ball_bottom.wpilib.json").getCommand(),
+    // new InstantCommand(m_intakeMotor::openIntake, m_intakeMotor),
+    // new PathConverter(m_drivetrain,
+    // "output/bottomtopark.wpilib.json").getCommand(),
+    // new InstantCommand(m_intakeMotor::closeIntake, m_intakeMotor)
+    // ));
 
-    m_chooser.addOption("Course 1 - WPI Trajectory", course1());
-    m_chooser.addOption("Course 2 - WPI Trajectory", course2());
-    m_chooser.addOption("Auto Routine Distance", new AutonomousDistance(m_drivetrain));
-    m_chooser.addOption("Auto Routine Time", new AutonomousTime(m_drivetrain));
-    
+    // m_chooser.addOption("Course 1 - WPI Trajectory", course1());
+    // m_chooser.addOption("Course 2 - WPI Trajectory", course2());
+    // m_chooser.addOption("Auto Routine Distance", new
+    // AutonomousDistance(m_drivetrain));
+    // m_chooser.addOption("Auto Routine Time", new AutonomousTime(m_drivetrain));
+
+    m_chooser.addOption("Ramsete Trajectory - park (0 ball)",
+        new PathConverter(m_drivetrain, "output/0ball.wpilib.json").getCommand());
     SmartDashboard.putData(m_chooser);
   }
 
@@ -203,7 +193,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     return m_chooser.getSelected();
-    
 
   }
 
@@ -213,7 +202,7 @@ public class RobotContainer {
    * @return the command to run in teleop
    */
   public Command getArcadeDriveCommand() {
-    return new ArcadeDrive(
-        m_drivetrain, () -> -m_controller.getRawAxis(1) * 0.8, () -> m_controller.getRawAxis(0) * 0.7);
+    return new ArcadeDrive(m_drivetrain, () -> -m_controller.getRawAxis(1) * 0.8,
+        () -> m_controller.getRawAxis(0) * 0.7);
   }
 }
